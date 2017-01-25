6ed394b909c50b2d77e9d38ee1153f6fa62acec6
Darwin Boaventura <cin_dboaventura@uolinc.com>
2017-01-25 13:23:13 -0200

    feito action editar page no angularjs

--- public/js/controllers/PageAdminController.js
+++ public/js/controllers/PageAdminController.js
@@ -1,11 +1,29 @@
 angular.module('dboaventura').controller('PageAdminController', function($scope, PageService, $routeParams) {
+	// Home Pages
 	PageService.query(function(pages) {
 		$scope.pages = pages;
 	});
 
+	// page editar
 	if ($routeParams.id) {
 		PageService.get({id: $routeParams.id}, function(page) {
 			$scope.page = page;
 		});
+
+		$scope.updatePage = function() {
+			PageService.save($scope.page, function() {
+				$scope.flashMessage = {
+					type: 'success',
+					message: 'A página foi atualizada com sucesso'
+				};
+			}, function(error) {
+				$scope.flashMessage = {
+					type: 'error',
+					message: 'Não foi possível enviar sua mensagem, tente novamente.'
+				};
+
+				console.log(error);
+			});
+		};
 	}
 });
